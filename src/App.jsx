5035ec612cf88b77ef57822b66b46d9b568feb8c
dota1g <alfredtheslugger@gmail.com>
2024-08-14 20:18:44 +0800

    aaasdasdasds

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import * as os from 'os';
+import os from 'os'
 const networkInterfaces = os.networkInterfaces();
 const ip = networkInterfaces['eth0'][0]['address'];
 
