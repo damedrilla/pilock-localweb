528279487556f2b503d2f6d3f218e91da4c7f701
damedrilla <fhaus.nozomi@gmail.com>
2024-10-20 15:52:59 +0800

    massive changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserView, MobileView } from "react-device-detect";
+import { createHash } from "crypto";
 export default function App() {
   const [currentSchedule, setCurrentSchedule] = useState([]);
   const [localMode, setLocalMode] = useState(false);
 
-  const getSchedule = async () => {
-    try {
-      axios
-        .get("http://" + window.location.hostname + ":5000" + "/schedule")
-        .then((response) => {
-          setCurrentSchedule(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+  // const getSchedule = async () => {
+  //   try {
+  //     axios
+  //       .get("http://" + window.location.hostname + ":5000" + "/schedule")
+  //       .then((response) => {
+  //         setCurrentSchedule(response.data);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // };
   const isCloudUp = async () => {
     try {
       axios
@@ -33,33 +34,55 @@ export default function App() {
     }
   };
 
-  useEffect(() => {
-    isCloudUp();
-    let interval = setInterval(() => {
-      isCloudUp();
-    }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  // useEffect(() => {
+  //   isCloudUp();
+  //   let interval = setInterval(() => {
+  //     isCloudUp();
+  //   }, 5000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+  function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
   async function get_da_input(formData) {
-    getSchedule();
-    const user_course = formData.get("schedule");
-    const input_faculty = formData.get("faculty");
-
+    let testMode = false;
+    let domain = testMode ? "localhost" : window.location.hostname;
+    const _password_ = formData.get("pass");
+    let rndInt = randomIntFromInterval(20, 100)
+    let salt = makeid(rndInt)
+    let da_hash = createHash("sha256").update(_password_).digest("hex");
     try {
-      if (
-        user_course === currentSchedule.subject &&
-        input_faculty === currentSchedule.instructor
-      ) {
-        axios.post("http://" + window.location.hostname + ":5000" + "/unlock");
-        alert(`Door unlocked!`);
-      } else {
-        alert(`Try again!`);
-      }
+      axios
+        .post(
+          "http://" + domain + ":5670" + `/catAsk/`, {
+            lebron : salt + da_hash,
+            curry : rndInt
+          }
+        )
+        .then((response) => {
+          console.log(response.status)
+          if (response.status === 200) {
+            alert(`Door unlocked!`);
+          } else if (response.status === 202){
+            alert(`Wrong password!`);
+          }
+        });
     } catch (err) {
-      console.log(err);
+      alert("Wrong password!");
     }
   }
 
@@ -93,65 +116,32 @@ export default function App() {
               </p>
               <p>Oh no! You forgot your ID card and there's no internet!</p>
             </div>
+            <MDBInput
+              className="mb-4"
+              type="password"
+              id="form2Example1"
+              label="Password"
+              name="pass"
+            />
+
+            <MDBBtn type="submit" className="mb-4" block>
+              Unlock the door!
+            </MDBBtn>
             <div className="text-center">
-              <p>This webpage is only viewable in mobile</p>
+              <p>If you don't know the password, please</p>
+              <p style={{ marginTop: -15 }}>
+                look for the Laboratory In-Charge or a technician for it.
+              </p>
+            </div>
+            <div className="text-center">
+              <p>Disabled button means there's an internet connection</p>
+              <p style={{ marginTop: -15 }}>
+                in the device. Use your ID card or the web system!
+              </p>
             </div>
           </form>
         </div>
       </BrowserView>
-      <MobileView>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <form action={get_da_input}>
-            <div className="text-center">
-              <p
-                style={{ fontWeight: "bold", color: "#03bafc", fontSize: 100 }}
-              >
-                Pi-Lock:{" "}
-              </p>
-              <p
-                style={{
-                  fontWeight: "bold",
-                  color: "black",
-                  fontSize: 40,
-                  marginTop: -50,
-                }}
-              >
-                Last Resort{" "}
-              </p>
-              <p>Oh no! You forgot your ID card and there's no internet!</p>
-            </div>
-            <MDBInput
-              className="mb-4"
-              type="text"
-              id="form2Example1"
-              label="Current schedule"
-              name="schedule"
-            />
-            <MDBInput
-              className="mb-4"
-              type="text"
-              id="form2Example2"
-              label="Faculty assigned"
-              name="faculty"
-            />
-
-            <MDBBtn type="submit" className="mb-4" disabled={!localMode} block>
-              Unlock the door!
-            </MDBBtn>
-            <div className="text-center">
-              <p>Disabled button means there's an internet connection</p>
-              <p style={{ marginTop: -15 }}>in the device. Use your ID card!</p>
-            </div>
-          </form>
-        </div>
-      </MobileView>
     </>
   );
 }
